a95108e79369459e40df62d32433e794034e0a25
a-keyter <106679998+a-keyter@users.noreply.github.com>
2022-06-02 09:47:06 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 console.log("Connected to app.js");
 
+var showExample = true;
+var deleteExampleBut = document.getElementById("deleteButton0");
+
 var currentTasks = function(){
-    return(document.getElementById("tasksContainer").childElementCount - 2);
+    if(showExample) {
+        return(document.getElementById("tasksContainer").childElementCount - 2);
+    } else {
+        return(document.getElementById("tasksContainer").childElementCount - 1);
+    }
 }
 
 var listCount = document.getElementById("listCount");
 
 listCount.innerText = currentTasks();
 
-
-
 var addTaskButton = document.getElementById("addTaskButton");
 
 var createNewTask = function () {
-    console.log("Button Pressed");
+    console.log("Task added");
 
-    var newTaskNumber = currentTasks.length;
+    var newTaskNumber = currentTasks() + 1;
     var newTaskText = document.getElementById("newToDoText").value;
     var newDueDate = document.getElementById("newDueDate").value;
-    
+    var newDeleteId = ("deleteButton" + newTaskNumber);
 
     var taskContainerEl = document.createElement("div");
     var taskNameEl = document.createElement("div");
@@ -33,12 +38,13 @@ var createNewTask = function () {
     
     taskDeleteEl.className += "centerMargin";
     taskDeleteEl.type += "button";
-    taskDeleteEl.id += ("deleteButton" + newTaskNumber);
+    taskDeleteEl.id += newDeleteId;
 
     taskNameEl.innerText = newTaskText;
     taskDateEl.innerText = newDueDate;
     taskDeleteEl.innerText = "X";
 
+    
     taskContainerEl.appendChild(taskNameEl);
     taskContainerEl.appendChild(taskDateEl);
     taskContainerEl.appendChild(taskDeleteEl);
@@ -47,6 +53,39 @@ var createNewTask = function () {
     taskList.appendChild(taskContainerEl);
 
     listCount.innerText = currentTasks();
+
+    // sets up new event listener specific to this delete item
+    var deleteButtonEl = document.getElementById(newDeleteId)
+    deleteButtonEl.addEventListener("click", deleteTask);
+}
+
+var deleteTask = function () {
+    //find the delete button id
+    var deleteId = this.id;
+
+    //get the last digit of the id name
+    var buttonNumber = deleteId.slice(-1);
+    
+    //set variable name for container
+    var findContainerId = "taskNumber" + buttonNumber;
+
+    //find element container
+    var deleteContainerEl = document.getElementById(findContainerId);
+
+    //delete / remove container element
+    deleteContainerEl.remove();
+    console.log(findContainerId + " has been deleted");
+
+    // update list counter
+    listCount.innerText = currentTasks();
+};
+
+var deleteExample = function () {
+    var exampleContainer = document.getElementById("taskNumber0");
+    exampleContainer.remove();
+    console.log("Example has been deleted");
+    showExample = false;
 };
 
 addTaskButton.addEventListener("click", createNewTask);
+deleteExampleBut.addEventListener("click", deleteExample);
